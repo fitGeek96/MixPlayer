311f0b0d6dc809948acf918ddc1ce15c680650bc
fitgeek96 <fitgeek96@gmail.com>
2020-07-29 10:17:53 +0100

    MixPlayer project starter files

---
+++ js/data/songs.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+
+export const songsList = [{
+    "id": "1",
+    "url": "../../music/ibrahim.mp3",
+    "time": "11:50",
+    "title": "قصيدة سيدنا إبراهيم",
+    "artist": "Meriem Benallal"
+  },
+  {
+    "id": "2",
+    "url": "../../music/lotf-allah.mp3",
+    "time": "05:46",
+    "title": "لطف الله الخافي",
+    "artist": "Meriem Benallal"
+  },
+  {
+    "id": "3",
+    "url": "../../music/maaya.mp3",
+    "time": "05:06",
+    "title": "معايا",
+    "artist": "Meriem Benallal"
+  },
+  {
+    "id": "4",
+    "url": "../../music/ya-moulati.mp3",
+    "time": "06:04",
+    "title": "يا مولاتي",
+    "artist": "Meriem Benallal"
+  },
+  
+];
